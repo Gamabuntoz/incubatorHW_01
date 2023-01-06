309ce8be9e2a4e556dd188f22239a85bc7a52bb5
Tsimofei <zhalianhouski@gmail.com>
2023-01-06 18:39:06 +0300

    init commit

--- src/routes/testingRouter.ts
+++ src/routes/testingRouter.ts
@@ -2,6 +2,6 @@ import {Request, Response, Router} from "express";
 import {videosRouter} from "./videosRouter";
 export const testingRouter = Router()
 
-videosRouter.delete('/:all-data', (req: Request, res: Response) => {
-    res.status(204).send('All data is deleted')
+testingRouter.delete('/:all-data', (req: Request, res: Response) => {
+    res.status(204)
 })
